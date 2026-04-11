32a954f5d331adbd8a5f1eae7a6a2c9ae9e349f6
alfhz <alfh.zachra@gmail.com>
2026-04-11 10:39:17 +0700

    fix: update srsitektur proyek

---
+++ src/utils/file_handler.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+def buka_json():
+    """Membuka database utama"""
+    path = "data/data_wisata.json"
+    if not os.path.exists(path): return []
+    with open(path, 'r') as f:
+        return json.load(f)
+
+def simpan_json(data):
+    """Menyimpan data terbaru ke database"""
+    path = "data/data_wisata.json"
+    os.makedirs("data", exist_ok=True)
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=4)
